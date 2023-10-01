02eb6bd18bb8190f490d69bcc02dd21fe0bc3ac8
Jared Black <j.black16@icloud.com>
2023-09-30 23:17:03 -0500

    Committing to new Repo

---
+++ socialProject/Reactivities/client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,39 @@
+import { Grid, GridColumn } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponents";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { observer } from "mobx-react-lite";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
+
+
+
+
+export default observer (function ActivityDetails() {
+
+  const {activityStore} = useStore();
+  const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+  const {id} = useParams();
+
+  useEffect(() => {
+    if(id) loadActivity(id);
+  }, [id, loadActivity])
+
+  if(loadingInitial || !activity) return <LoadingComponent content={"Loading..."} />;
+
+    return (
+      <Grid>
+        <GridColumn width={10}>
+          <ActivityDetailedHeader activity={activity}/>
+          <ActivityDetailedInfo activity={activity}/>
+          <ActivityDetailedChat />
+        </GridColumn>
+        <GridColumn width={6}>
+          <ActivityDetailedSidebar />
+        </GridColumn>
+      </Grid>
+    )
+})
