8bbee4cadc52d09327dcc480e1b063315f8966b8
Jared Black <j.black16@icloud.com>
2023-10-11 19:54:16 -0500

    Added Frontend Login page

--- socialProject/Reactivities/client-app/src/app/api/agent.ts
+++ socialProject/Reactivities/client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
+import { User, UserFormValues } from "../models/user";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -53,7 +54,7 @@ axios.interceptors.response.use(async response => {
 
 const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
-const request = {
+const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody), 
     post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody), 
     put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody), 
@@ -61,15 +62,22 @@ const request = {
 }
 
 const Activities = {
-    list: () => request.get<Activity[]>('/activities'),
-    details: (id: string) => request.get<Activity>(`/activities/${id}`),
+    list: () => requests.get<Activity[]>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: Activity) => axios.post<void>('/activities', activity),
     update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    regiser: (user: UserFormValues) => requests.post<User>('/account/register', user) 
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
